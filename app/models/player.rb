d497df8f90dd1e9afbc4451f9aae21dbd7724a25
Emmanuel Hayford <siawmensah@gmail.com>
2020-02-17 21:47:23 +0100

    Display a default picture if Heroku restarts server

--- app/models/player.rb
+++ app/models/player.rb
@@ -4,4 +4,8 @@ class Player < ApplicationRecord
   attribute :player_picture, :string, default: ''
 
   has_one_attached :picture
+
+  def has_picture?
+    File.exist?(ActiveStorage::Blob.service.path_for(picture.key))
+  end
 end
